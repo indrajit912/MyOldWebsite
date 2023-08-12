d0712e7a9d958572fef5466ac4200a5d5d785c18
Indrajit <indrajitghosh912@gmail.com>
2023-08-12 16:34:23 +0530

    teaching details added

--- app/routes.py
+++ app/routes.py
@@ -11,6 +11,7 @@ Attributes:
 from flask import render_template
 from app import app
 
+# Home
 @app.route('/')
 def index():
     """
@@ -20,4 +21,19 @@ def index():
         str: The rendered HTML template for the index page.
     """
     return render_template('index.html')
+
+
+# Teaching
+@app.route('/teaching')
+def teaching():
+    return render_template('teaching.html')
+
+@app.route('/teaching/first.html')
+def first_teaching():
+    return render_template('teaching/first.html')
+
+# Research
+@app.route('/research')
+def research():
+    return render_template('research.html')
 
